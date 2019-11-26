dca7bf7e0e7df51a5f514c5257b1a10df323b060
teste <teste@edu.frei>
2019-11-26 14:17:44 -0300

    José Rodrigo : ProfessorDatabase criado

---
+++ Database/ProfessorDatabase.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+
+namespace Vivencia19TardeAPI.Database
+{
+    public class ProfessorDatabase
+    {
+
+        public void Inserir(Models.TbProfessor professor)
+        {
+            
+        }
+
+        
+        
+    }
+}
