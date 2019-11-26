c5e3f3c5ae594def10c1eaeafd3ec319b9515345
teste <teste@edu.frei>
2019-11-26 14:59:13 -0300

    José Rodrigo : ProfessorDatabase Certo

--- Database/ProfessorDatabase.cs
+++ Database/ProfessorDatabase.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 
@@ -7,11 +9,46 @@ namespace Vivencia19TardeAPI.Database
 {
     public class ProfessorDatabase
     {
+        Models.db_a5064d_freiContext db = new Models.db_a5064d_freiContext();
 
-        public void Inserir(Models.TbProfessor professor)
+        public void Inserir(Models.TbProfessor prof)
         {
-            
+             db.Add(prof);
+             db.SaveChanges();
         }
+
+        public List<Models.TbProfessor> ListarTodos()
+        {
+            List<Models.TbProfessor> professor = db.TbProfessor.ToList();
+
+            return professor;
+        }
+        public List<Models.TbProfessor> ConsultarPorNome(string nome)
+        {
+            List<Models.TbProfessor> professor = db.TbProfessor.Where(t => t.NmProfessor.Contains(nome)).ToList();
+
+            return professor;
+        }
+
+        public void Remover(int id)
+        {
+             db.Remove(id);
+             db.SaveChanges();
+        }
+
+        public void Alterar(Models.TbProfessor prof)
+        {
+            Models.TbProfessor alterado = db.TbProfessor.FirstOrDefault(t => t.IdProfessor == prof.IdProfessor);
+
+
+
+        }
+
+
+
+
+
+
 
         
 
