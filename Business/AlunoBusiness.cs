f5026de6d8048b2a5e10a21f5fde6ca70f3b2cbe
teste <teste@edu.frei>
2019-11-26 17:28:13 -0300

    Aluno database e validações

--- Business/AlunoBusiness.cs
+++ Business/AlunoBusiness.cs
@@ -38,10 +38,34 @@ namespace Vivencia19TardeAPI.Business
 
         private void validacao (Models.TbAluno aluno)
         {
-            Models.TbAluno Individuo = DB.Carregar(aluno);
 
-            // if (aluno.DsCpf ==Individuo.DsCpf && )
-            // throw new ArgumentException("Este")
+            Models.TbAluno Individuo = DB.Carregar(aluno);
+            int diferença = Convert.ToInt32(DateTime.Now.Year - aluno.DtNascimento.Year);
+
+            if (aluno.DsCpf.Length != 14 && aluno.DsCpf == null)
+            throw new ArgumentException ("Este CPF esta invalido");
+
+            else if (aluno.DsEmail.Contains("@") == false && aluno.DsEmail.Contains(".com") == false)
+            throw new ArgumentException ("Este Email esta invalido");
+
+            else if (aluno.DsEmail == string.Empty)
+            throw new ArgumentException ("O campo de Email é obrigatorio");
+
+            else if (aluno.DsRg.Length != 10)
+            throw new ArgumentException("O RG esta invalido");
+
+            else if (aluno.DsRg == string.Empty)
+            throw new ArgumentException("O campo de RG é obrigatorio");
+
+            else if (aluno.DtEmissao == null)
+            throw new ArgumentException("O campo de Emissão é obrigatorio");
+
+            else if (aluno.DtNascimento == null)
+            throw new ArgumentException("O campo de Data de Nascimento é obrigatorio");
+
+            
+            else if (diferença <=14 || diferença >=25 )
+            throw new ArgumentException("O individuo não tem a idade necessária para ingressar no curso");
         }
     }
 }
