62b472550980076473dc4d0cd931f619cbc93bd1
teste <teste@edu.frei>
2019-11-26 16:14:52 -0300

    Mayara : Database feito e terminando business

---
+++ Database/TbAlunoLocalizacaoDatabase.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Linq.Expressions;
+using Vivencia19TardeAPI.Models;
+
+namespace Vivencia19TardeAPI.Database
+{
+    public class TbAlunoLocalizacaoDatabase
+    {
+        Models.db_a5064d_freiContext db = new Models.db_a5064d_freiContext();
+        public void Inserir(Models.TbAlunoLocalizacao ModelAlunoLocalizacao)
+        {
+            db.Add(ModelAlunoLocalizacao);
+        }
+
+        public List<Models.TbAlunoLocalizacao> Consultar()
+        {
+            List<Models.TbAlunoLocalizacao> Lista = db.TbAlunoLocalizacao.ToList();
+
+            return Lista;
+        } 
+
+        public void Alterar(int id,TbAlunoLocalizacao local)
+        {
+            Models.TbAlunoLocalizacao alterar = db.TbAlunoLocalizacao.FirstOrDefault(t => t.IdAluno == id);
+
+            alterar.DsNascimentoEstado = local.DsNascimentoEstado;
+            alterar.DsNascimentoPais = local.DsNascimentoPais;
+            alterar.DsNascimentoCidade = local.DsNascimentoCidade;
+            alterar.DsResidenciaBairro = local.DsResidenciaBairro;
+            alterar.DsResidenciaCep = local.DsResidenciaCep;
+            alterar.DsResidenciaCidade = local.DsResidenciaCidade;
+            alterar.DsResidenciaComplelemento = local.DsResidenciaComplelemento;
+            alterar.DsResidenciaEndereco = local.DsResidenciaEndereco;
+            alterar.DsResidenciaEstado = local.DsResidenciaEstado;
+            alterar.DtInclusao = local.DtInclusao;
+            alterar.DtUltimaAlteracao= local.DtUltimaAlteracao;
+            alterar.NrResidenciaEndereco= local.NrResidenciaEndereco;
+
+            db.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            Models.TbAlunoLocalizacao remover = db.TbAlunoLocalizacao.FirstOrDefault(a => a.IdAluno == id);
+
+            db.Remove(remover);
+        }
+    }
+}
---
+++ Database/TbAlunoMensalidadeDatabase.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Linq.Expressions;
+using Vivencia19TardeAPI.Models;
+
+namespace Vivencia19TardeAPI.Database
+{
+    public class TbAlunoMensalidadeDatabase
+    {
+        Models.db_a5064d_freiContext db = new Models.db_a5064d_freiContext();
+        public void Inserir(Models.TbAlunoMensalidade ModelAlunoMensalidade)
+        {
+            db.Add(ModelAlunoMensalidade);
+        }
+
+        public List<Models.TbAlunoMensalidade> Consultar()
+        {
+            List<Models.TbAlunoMensalidade> Lista = db.TbAlunoMensalidade.ToList();
+
+            return Lista;
+        } 
+
+        public void Alterar(int id,TbAlunoMensalidade mensalidade)
+        {
+            Models.TbAlunoMensalidade alterar = db.TbAlunoMensalidade.FirstOrDefault(t => t.IdAluno == id);
+
+            alterar.DsEmail = mensalidade.DsEmail;
+            alterar.DsParentesco = mensalidade.DsParentesco;
+            alterar.DsRg = mensalidade.DsRg;
+            alterar.DsTelefone1 = mensalidade.DsTelefone1;
+            alterar.DsTelefone2 = mensalidade.DsTelefone2;
+            alterar.DtInclusao = mensalidade.DtInclusao;
+            alterar.DtUltimaAlteracao = mensalidade.DtUltimaAlteracao;
+            alterar.NmResponsavel = mensalidade.NmResponsavel;
+
+            db.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            Models.TbAlunoMensalidade remover = db.TbAlunoMensalidade.FirstOrDefault(a => a.IdAluno == id);
+
+            db.Remove(remover);
+        }
+    }
+}
