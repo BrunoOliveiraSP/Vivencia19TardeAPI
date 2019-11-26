c529c1879574d2c2cb3ef053fe665ab82e4fc454
Windows <Windows@WINDOWS-PC>
2019-11-26 17:01:59 -0300

    Gabriel Rocha: Sala Controller

--- Controllers/SalaController.cs
+++ Controllers/SalaController.cs
@@ -1,7 +1,32 @@
-namespace Vivencia19TardeAPI_1.Controllers
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Vivencia19TardeAPI.Controllers
 {
-    public class SalaController
+    [ApiController]
+    [Route("[controller]")]
+    public class SalaController : ControllerBase
     {
-        
+        Business.SalaBusiness business = new Business.SalaBusiness();
+
+        [HttpPost("Inserir")]       
+         public void Inserir(Models.TbSala sala)
+        {
+            business.Inserir(sala);
+        }
+
+        [HttpGet("ListarTudo")]      
+        public List<Models.TbSala> ListarTudo()
+        {
+            List<Models.TbSala> sala = business.ListarTudo();
+            return sala;
+
+        }
+                
     }
 }
