56aedb6a349e4723650861cdeff0fe4ad0abb826
teste <teste@edu.frei>
2019-11-26 16:18:39 -0300

    GabrielBrandao Infoc 11, Business e Database :Alunos, documentos, responsavel, matricula

---
+++ Database/ResponsavelDatabase.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Vivencia19TardeAPI.Database
+{
+    
+    public class ResponsavelDatabase
+    {
+        Models.db_a5064d_freiContext DB = new Models.db_a5064d_freiContext();
+
+       
+    }
+}
