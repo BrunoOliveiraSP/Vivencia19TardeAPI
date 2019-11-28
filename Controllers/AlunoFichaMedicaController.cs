54a4440640e877d0acf965fccb81e5e7039e4bfb
Thiago Nascimento <thiago.jem@hotmail.com>
2019-11-28 20:18:44 -0300

    Thiago: Arrumei um controller

--- Controllers/AlunoFichaMedicaController.cs
+++ Controllers/AlunoFichaMedicaController.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Vivencia19TardeAPI.Models;
 
 namespace Vivencia19TardeAPI.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class AlunoFichaMedicaController
     {
         Business.AlunoFichaMedicaBusiness Bus = new Business.AlunoFichaMedicaBusiness();
@@ -17,15 +23,17 @@ namespace Vivencia19TardeAPI.Controllers
          [HttpGet]
          public List<Models.TbAlunoFichaMedica> Consultar()
          {
-             List<Models.TbAlunoFichaMedica> lista= Bus.Consultar();
+             List<Models.TbAlunoFichaMedica> lista = Bus.Consultar();
              return lista;
          }
+
          [HttpDelete]
          public void Deletar(int id)
          {
              Bus.Deletar(id);
              
          }
+         
          [HttpPut]
          public void Alterar(int id,TbAlunoFichaMedica fichaMedica)
          {
--- Controllers/AlunoMensalidadeController.cs
+++ Controllers/AlunoMensalidadeController.cs
@@ -1,5 +1,9 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Vivencia19TardeAPI.Models;
 
 namespace Vivencia19TardeAPI.Controllers
@@ -8,7 +12,7 @@ namespace Vivencia19TardeAPI.Controllers
     [Route("[controller]")]
     public class AlunoMensalidadeController
     {
-      Business.AlunoMensalidadeBusiness Bus = new Business.AlunoMensalidadeBusiness();
+        Business.AlunoMensalidadeBusiness Bus = new Business.AlunoMensalidadeBusiness();
         
         [HttpPost]
         public void Inserir(TbAlunoMensalidade mensalidade)
@@ -19,20 +23,21 @@ namespace Vivencia19TardeAPI.Controllers
          [HttpGet]
          public List<TbAlunoMensalidade> Consultar()
          {
-             List<TbAlunoMensalidade> lista= Bus.Consultar();
+             List<TbAlunoMensalidade> lista = Bus.Consultar();
              return lista;
          }
+         
          [HttpDelete]
          public void Deletar(int id)
          {
              Bus.Deletar(id);
              
          }
+
          [HttpPut]
          public void Alterar(int id,TbAlunoMensalidade mensalidade)
          {
              Bus.Alterar(id, mensalidade);
          }
-      
     }
 }
