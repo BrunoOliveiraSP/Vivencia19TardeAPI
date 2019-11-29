c8c35714f014fc727fbf05ad09b3ebed9bb8f14b
teste <teste@edu.frei>
2019-11-29 15:31:46 -0300

    ControllerTurma

--- Business/TurmaAnoLetivo.cs
+++ Business/TurmaAnoLetivo.cs
@@ -5,9 +5,29 @@ using System.Linq;
 using System.Linq.Expressions;
 
 namespace Vivencia19TardeAPI.Business
-{
+{ 
+       
     public class TurmaAnoLetivo
     {
-        
+        Database.TurmaAnoLetivoDatabase db = new Database.TurmaAnoLetivoDatabase();
+        public void Inserir (Models.TbTurma turma)
+        {
+           db.CadastrarTurma(turma);
+        }
+
+        public List<Models.TbTurma> ListarTodos()
+        {
+           List<Models.TbTurma> lista = db.ListarTodos();
+           return lista;
+        }
+
+        public void Alterar(Models.TbTurma turma)
+        {
+            db.Alterar(turma);
+        }
+        public void Remover(int id)
+        {
+            db.Remover(id);
+        }
     }
 }
