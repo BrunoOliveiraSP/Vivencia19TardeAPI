7edc176d860e4ea44d788807961167489381d270
Bruno de Oliveira <brunooliveira.me@outlook.com>
2019-12-10 14:19:10 -0300

    ...

--- Database/InscricaoDatabase.cs
+++ Database/InscricaoDatabase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections;
 using System.Linq;
 using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Vivencia19TardeAPI.Database
 {
